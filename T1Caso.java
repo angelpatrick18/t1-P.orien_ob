da3780fb24c4aa61866a6c8adbf881d39962dd5b
Angel patricio <n00442769@upn.pe>
2026-09-21 20:11:52 -0500

    mi primer comit_t1

---
+++ T1Caso.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t1.caso;
+
+/**
+ *
+ * @author UCA40425
+ */
+public class T1Caso {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
