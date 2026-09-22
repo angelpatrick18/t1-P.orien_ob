f7c9113bf10ab6097b165ce8745ca861e0f4653a
Angel patricio <n00442769@upn.pe>
2026-09-21 20:19:44 -0500

    segundo intento con lprivate y encapsulamiento

--- alumno.java
+++ alumno.java
@@ -9,5 +9,71 @@ package t1.caso;
  * @author UCA40425
  */
 public class alumno {
+    private String nombre;
+    private String tip_doc;
+    private String num_doc;
+    private String tip_beca;
+    private String direccion;
+    private String telefono;
+    private String cod_estudiante;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTip_doc() {
+        return tip_doc;
+    }
+
+    public void setTip_doc(String tip_doc) {
+        this.tip_doc = tip_doc;
+    }
+
+    public String getNum_doc() {
+        return num_doc;
+    }
+
+    public void setNum_doc(String num_doc) {
+        this.num_doc = num_doc;
+    }
+
+    public String getTip_beca() {
+        return tip_beca;
+    }
+
+    public void setTip_beca(String tip_beca) {
+        this.tip_beca = tip_beca;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCod_estudiante() {
+        return cod_estudiante;
+    }
+
+    public void setCod_estudiante(String cod_estudiante) {
+        this.cod_estudiante = cod_estudiante;
+    }
     
+    
+    
+            
 }
